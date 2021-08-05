11f49c15e924220e7d7e9aec36c687962ecbcb9c
mjzxin <MJZXIN@outlook.com>
2021-08-05 08:33:04 +0800

    添加项目文件。

---
+++ API/sockets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace API
+{
+    public class sockets
+    {
+        static Socket soc = null;
+        static bool isRunning = false;
+
+    }
+}
